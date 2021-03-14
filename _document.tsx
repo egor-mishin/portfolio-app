f7868bbe23b6223417465bf7f648e07c0733152b
Egor Mishin <em@egormishin.com>
2021-03-14 22:58:51 +0300

    initial commit

---
+++ _document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="icon" href="/favicon.ico" />
+
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800&display=swap"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
