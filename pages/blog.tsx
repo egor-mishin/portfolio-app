be532ff3f9507b24317d48e430672e3406419efb
Egor Mishin <em@egormishin.com>
2021-03-22 21:45:55 +0300

    feature: pages added

---
+++ pages/blog.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { FC } from "react"
+
+type Props = {}
+
+const Blog: FC<Props> = (): JSX.Element => {
+	return <>Blog</>
+}
+
+export default Blog
---
+++ pages/portfolio.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { FC } from "react"
+
+type Props = {}
+
+const Portfolio: FC<Props> = (): JSX.Element => {
+	return <>Portfolio</>
+}
+
+export default Portfolio
