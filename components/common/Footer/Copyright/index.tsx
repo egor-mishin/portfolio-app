bace8354469c5e53d4d66bc40577df4a8c76e278
Egor Mishin <em@egormishin.com>
2021-03-23 20:49:04 +0300

    feat: implement copyright component

---
+++ components/common/Footer/Copyright/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {FC} from "react";
+
+type Props = {
+
+};
+
+const  Copyright: FC<Props> = (): JSX.Element => {
+    return (
+        <>Copyright</>
+    )
+}
+
+export default  Copyright
