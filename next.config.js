41f3182b689aa4542c8ca00df279ca61cd06755b
Egor Mishin <em@egormishin.com>
2021-04-07 14:26:09 +0300

    refactor: add same settings

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 module.exports = {
 	images: {
 		domains: ['res.cloudinary.com'],
-	}
+		loader: 'cloudinary'
+	},
+
+	exportPathMap: async function (
+		defaultPathMap,
+		{ dev, dir, outDir, distDir, buildId }
+	) {
+		return {
+			'/': { page: '/' },
+			'/blog': { page: '/blog' },
+			'/portfolio': { page: '/portfolio' },
+		}
+	},
+
 }
