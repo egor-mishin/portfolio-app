fcee44e2f51f93f381c6286c5046a3fb8fc2ccb2
Egor Mishin <em@egormishin.com>
2021-04-07 15:55:29 +0300

    refactor: images config refactoring

---
+++ components/common/Image/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Image from 'next/image'
+
+type Props = {
+    
+};
+
+const myLoader = ({ src, width, quality }) => {
+  return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+}
+
+const MyImage = (props) => {
+  return (
+    <Image
+      loader={myLoader}
+      src="/me.png"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+    />
+  )}
